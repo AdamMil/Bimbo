cc42d9a58f3c687fc56bae77cdd634dd7c3c49a6
adam <adam@6911f97f-54ec-0310-8281-aee29118f4a8>
2004-03-01 08:52:05 +0000

    *** empty log message ***

--- trunk/BimboApp/Objects.cs
+++ trunk/BimboApp/Objects.cs
@@ -16,7 +16,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+using System;
 using Bimbo;
+using GameLib.Input;
 using GameLib.Interop.OpenGL;
 using GameLib.Mathematics.TwoD;
 
@@ -26,13 +28,45 @@ namespace Bimbo.Objects
 public class Coin : BimboObject
 { public Coin(List list) : base(list) { sprite = Sprite.Load("coin.sps"); }
 
-  public override void Render(World world) { DrawSprite(world.Camera, sprite, Anim.GetFrame(AnimPos)); }
-  public override void Update(World world) { }
+  public override void Render() { DrawSprite(sprite, Anim.GetFrame(AnimPos)); }
+  public override void Update() { }
 
   Sprite sprite;
 
   internal static Animation.Position AnimPos;
   internal static Animation Anim = new Animation(new Animation.Chunk(0,  8, 0.10f));
+}
+
+public class Player : BimboObject
+{ public Player(List list) : base(list) { sprite = Sprite.Load("swarmie.png"); me=this; }
+
+  public override void Render() { DrawSprite(sprite, 0); }
+
+  public override void Update()
+  { int accel = ;
+    if(vel.X!=0 && (accel==0 || Math.Abs(Math.Sign(accel)-Math.Sign(vel.X))==2))
+    { vel.X = (Math.Abs(vel.X) - friction*weight*world.TimeDelta) * Math.Sign(vel.X);
+      if(Math.Abs(vel.X)<StopSpeed) vel.X=0;
+      Console.WriteLine(vel.X);
+    }
+    if(accel!=0)
+      vel.X += Math.Sign(accel) * Math.Min(Math.Abs(accel), MaxSpeed-Math.Abs((int)vel.X)) * world.TimeDelta;
+
+    pos += vel * world.TimeDelta;
+  }
+  
+  public float MaxWalkingSpeed { get { return 600; } }
+
+  public float WalkingAccel
+  { get { return (Keyboard.Pressed(Key.Left)?-Accel:0) + (Keyboard.Pressed(Key.Right)?Accel:0); }
+  }
+  
+  public float 
+  public const int StopSpeed=16, Accel=1000, MaxSpeed=600;
+
+  Sprite sprite;
+
+  public static Player me;
 }
 
 }
